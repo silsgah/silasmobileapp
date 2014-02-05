4c353df36f08c614eb37c767338df4a8e5cc6705
silsgah <silasgah@yahoo.co.uk>
2014-02-04 16:54:13 -0800

    sample
    sample

---
+++ Jitterz Mobile/scripts/App/app.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="js/jquery.min.js" />
+/// <reference path="js/kendo.all-vsdoc.js" />
+
+
+
+//window.kendoMobileApplication = new kendo.mobile.Application(document.body, { skin: "jitterz-default" });
+var app = new kendo.mobile.Application(document.body, { transition: "slide", layout: "jitterz-default" });
+var dataSourcelist = new kendo.data.DataSource({
+    transport: {
+        read: {
+            url: 'http://localhost:29098/api/sample',
+            ConentType: "json"
+        }
+    }
+});
+$("#itemslist").kendoMobileListView({ dataSource: dataSourcelist,
+    template: $("#template").text()
+});
+
+
+
+
+
+
